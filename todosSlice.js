df8e8dbcad7d7bbf7efa0ef109778c59a3ebd539
VISWANATHAN. S <142203206+viswavich@users.noreply.github.com>
2024-07-31 17:34:51 +0530

    Add files via upload

---
+++ todosSlice.js
@@ -0,0 +1,24 @@
+// src/features/todos/todosSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({ id: Date.now(), text: action.payload, completed: false });
+    },
+    removeTodo: (state, action) => {
+      return state.filter(todo => todo.id !== action.payload);
+    },
+    toggleComplete: (state, action) => {
+      const todo = state.find(todo => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleComplete } = todosSlice.actions;
+export default todosSlice.reducer;
